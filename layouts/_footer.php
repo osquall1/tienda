a13abe6702bdcf386d679d0cca85488feb3398bd
osquall1 <42726048+osquall1@users.noreply.github.com>
2021-11-25 10:58:48 -0500

    carpetas

---
+++ layouts/_footer.php
@@ -0,0 +1,17 @@
+<footer>
+	<div class="body-footer">
+		<div class="div-flex mb20">
+			<div class="part">
+				<h4>Sistema de prueba </h4>
+				<p>Gym Max </p>
+			</div>
+			<div class="part">
+				<h4 class="mb10">Redes sociales</h4>
+				<a href="" target="_blank">Facebook: Gym Max</a>
+				<br>
+				<a href="" target="_blank">Whatsapp: 3194158400</a>
+			</div>
+		</div>
+		<center><h5>Desarrollado por Oscar Manrique - Derechos reservador 2021</h5></center>
+	</div>
+</footer>
